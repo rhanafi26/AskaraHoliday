e8729ea1e2710e16a695f6fe605c2e8931d6f71c
rhanafi26@gmail.com <rizahanafi98@gmail.com>
2026-08-10 15:01:31 +0700

    fix config

--- vite.config.js
+++ vite.config.js
@@ -4,32 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
     chunkSizeWarningLimit: 1000,
-    rollupOptions: {
-      output: {
-        // Gunakan function untuk manualChunks (format yang kompatibel)
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('react') || id.includes('react-dom')) {
-              return 'react-vendor'
-            }
-            if (id.includes('framer-motion')) {
-              return 'motion'
-            }
-            if (id.includes('react-icons')) {
-              return 'icons'
-            }
-            return 'vendor'
-          }
-        },
-      },
-    },
   },
 })
